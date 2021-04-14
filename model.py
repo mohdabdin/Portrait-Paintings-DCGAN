0cc059688c6aabd4b92b6ad88ed9c76d64680121
Mohammad Abdin <72082608+mohdabdin@users.noreply.github.com>
2021-04-15 00:02:46 +0100

    Update and rename DCGAN_model.py to model.py

--- model.py
+++ model.py
@@ -1,7 +1,3 @@
-"""
-Discriminator and Generator implementation from DCGAN paper
-"""
-
 import torch
 import torch.nn as nn
 
@@ -11,35 +7,36 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.disc = nn.Sequential(
             # input: N x channels_img x 64 x 64
-            nn.Conv2d(
-                channels_img, features_d, kernel_size=4, stride=2, padding=1
-            ),
+            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2),
             # _block(in_channels, out_channels, kernel_size, stride, padding)
             self._block(features_d, features_d * 2, 4, 2, 1),
             self._block(features_d * 2, features_d * 4, 4, 2, 1),
             self._block(features_d * 4, features_d * 8, 4, 2, 1),
             # After all _block img output is 4x4 (Conv2d below makes into 1x1)
-            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
+        )
+        self.output = nn.Sequential(
             nn.Sigmoid(),
         )
 
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
             nn.Conv2d(
-                in_channels,
-                out_channels,
-                kernel_size,
-                stride,
-                padding,
-                bias=False,
+                in_channels, out_channels, kernel_size, stride, padding, bias=False,
             ),
-            #nn.BatchNorm2d(out_channels),
+            nn.InstanceNorm2d(out_channels, affine=True),
             nn.LeakyReLU(0.2),
         )
 
-    def forward(self, x):
-        return self.disc(x)
+    def forward(self, x, feature_matching = False):
+        features = self.disc(x)
+        output = self.output(features)
+        
+        if feature_matching:
+            return features.view(-1, 512*4*4), output
+        else:
+            return output
+
 
 
 class Generator(nn.Module):
@@ -52,7 +49,7 @@ class Generator(nn.Module):
             self._block(features_g * 8, features_g * 4, 4, 2, 1),  # img: 16x16
             self._block(features_g * 4, features_g * 2, 4, 2, 1),  # img: 32x32
             nn.ConvTranspose2d(
-                features_g * 2, channels_img, kernel_size=4, stride=4, padding=0
+                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
             ),
             # Output: N x channels_img x 64 x 64
             nn.Tanh(),
@@ -61,14 +58,9 @@ class Generator(nn.Module):
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
             nn.ConvTranspose2d(
-                in_channels,
-                out_channels,
-                kernel_size,
-                stride,
-                padding,
-                bias=False,
+                in_channels, out_channels, kernel_size, stride, padding, bias=False,
             ),
-            #nn.BatchNorm2d(out_channels),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
 
@@ -81,14 +73,3 @@ def initialize_weights(model):
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(m.weight.data, 0.0, 0.02)
-
-def test():
-    N, in_channels, H, W = 8, 3, 64, 64
-    noise_dim = 100
-    x = torch.randn((N, in_channels, H, W))
-    disc = Discriminator(in_channels, 8)
-    assert disc(x).shape == (N, 1, 1, 1), "Discriminator test failed"
-    gen = Generator(noise_dim, in_channels, 8)
-    z = torch.randn((N, noise_dim, 1, 1))
-    print(gen(z).shape)
-    assert gen(z).shape == (N, in_channels, H, W), "Generator test failed"
